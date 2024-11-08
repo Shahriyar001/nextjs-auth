46fcad9cd8d2f4f3c57c73866be711fc1bdd1ca3
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-08 22:28:03 +0600

    Get a logged-in user info from the session

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+type UserProps {
+  user?:{
+    name: string | null | undefined;
+    email: string | null | undefined;
+    image: string | null | undefined;
+  }
+}
 
-const Navbar = () => {
+const Navbar = ({session}:{session: UserProps | null}) => {
   return (
     <div className="navbar bg-base-100  border-b  w-[90%] mx-auto">
       <div className="navbar-start">
