d31c14a5317cd9a39005fafd163db3e85e4b248b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-09 19:30:17 +0600

    import google authentication

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,13 +1,15 @@
+"use client";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
-type UserProps {
-  user?:{
+type UserProps = {
+  user?: {
     name: string | null | undefined;
     email: string | null | undefined;
     image: string | null | undefined;
-  }
-}
+  };
+};
 
-const Navbar = ({session}:{session: UserProps | null}) => {
+const Navbar = ({ session }: { session: UserProps | null }) => {
   return (
     <div className="navbar bg-base-100  border-b  w-[90%] mx-auto">
       <div className="navbar-start">
@@ -66,16 +68,21 @@ const Navbar = ({session}:{session: UserProps | null}) => {
         </ul>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-error btn-outline text-white rounded-full px-5">
-          Logout
-        </button>
-
-        <Link
-          href="/login"
-          className="btn btn-accent btn-outline text-white rounded-full px-5"
-        >
-          Login
-        </Link>
+        {session?.user ? (
+          <button
+            onClick={() => signOut()}
+            className="btn btn-error btn-outline text-white rounded-full px-5"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link
+            href="/login"
+            className="btn btn-accent btn-outline text-white rounded-full px-5"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
